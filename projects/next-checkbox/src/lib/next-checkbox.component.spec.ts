7e7a98d065e74e3b3fae50ed71162744795ae9f8
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-02-20 15:14:28 +0300

    host component added

--- projects/next-checkbox/src/lib/next-checkbox.component.spec.ts
+++ projects/next-checkbox/src/lib/next-checkbox.component.spec.ts
@@ -1,19 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NextCheckboxComponent } from './next-checkbox.component';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <div class="container">
+      <next-checkbox
+        [disabled]="disabled"
+        [required]="required"
+        [tabIndex]="tabIndex"
+        [id]="id"
+      >
+        <span>Dog (426)</span>
+      </next-checkbox>
+      <input type="submit" />
+    </div>
+  `
+})
+class HostComponent {
+  disabled: boolean;
+  required: boolean;
+  tabIndex: number;
+  id: string;
+}
 
 describe('NextCheckboxComponent', () => {
   let component: NextCheckboxComponent;
   let fixture: ComponentFixture<NextCheckboxComponent>;
+  let hostFixture: ComponentFixture<HostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NextCheckboxComponent ]
-    })
-    .compileComponents();
+      declarations: [NextCheckboxComponent, HostComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    hostFixture = TestBed.createComponent(HostComponent);
     fixture = TestBed.createComponent(NextCheckboxComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
